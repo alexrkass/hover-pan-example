e65069a2cb0df13f3831bad0d1966a561a089387
Alex Kass <alexrkass@gmail.com>
2017-03-09 14:58:20 -0800

    remove button

--- banner.js
+++ banner.js
@@ -1,5 +1,4 @@
 //Fallback for mobile
-function{
     if(window.innerWidth <= 1080){
         var image = document.createElement("a-sky");
         image.setAttribute("src","background.jpg");
@@ -8,10 +7,9 @@ function{
         var vid = document.getElementById("banner-video");
         vid.parentNode.removeChild(vid);
     }
-}();
 
 // Remove VR button
-function{
+
     var button = document.getElementsByClassName('a-enter-vr-button')[0];
     button.parentNode.removeChild(button);
-}()
+
